aaf93c03bda6180288fccd129c2734a52cb4bcf7
Guilherme Almeida Tavares <guilherme.erosoft@hotmail.com>
2025-01-07 14:00:00 -0300

    ✨ Correções

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { Provider } from "./components/ui/provider"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
